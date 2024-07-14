f8a333297dfa8005c195044a240df1c509bfa4d7
Somesh Vijay <you@example.com>
2024-07-14 17:47:08 +0530

    XML based bean injection to classes

---
+++ src/main/java/org/someshspringrameworkDemo/Doctor.java
@@ -0,0 +1,24 @@
+package org.someshspringrameworkDemo;
+
+public class Doctor implements Staff {
+
+    private String qualification;
+
+    public Doctor(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public void assist(){
+        System.out.println("Doctor is assisting");
+    }
+
+    public String getQualification(){
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+
+}
---
+++ src/main/java/org/someshspringrameworkDemo/Staff.java
@@ -0,0 +1,5 @@
+package org.someshspringrameworkDemo;
+
+public interface Staff {
+    void assist();
+}
